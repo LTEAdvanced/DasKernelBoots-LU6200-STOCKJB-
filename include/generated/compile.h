1b1da9da03437a73f5b5dfbac0172a9c6c6b6c97
LTEAdvanced <exca456@naver.com>
2013-09-01 22:37:02 +0900

    real commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 29 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#29 SMP PREEMPT Sun Sep 1 20:10:55 KST 2013"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "harold-virtual-machine"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
